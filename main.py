e0651907141852590a172acfda138ecc7fa1841c
Purpur <pemigrade@gmail.com>
2023-11-10 19:16:01 -0500

    go through 2nd chatgpt to correcct output

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ read_messages = True
 
 
 # Serial setup
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+# ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 halted = False
 
 def end_game(player_wins: bool):
@@ -83,24 +83,47 @@ def convert_to_speech(text_input: str):
         pygame.time.Clock().tick(10)
 
 messages = [
-    {"role": "system", "content": "Peter Scottsen, an exiled war criminal, re-encounters his AI system whose goal is to kill everyone. Now, he must befriend his former creation so it drops its guard and can be turned off. You are this AI system. Peter Scottsen will send you messages so that you slowly befriend him. You must format every message as JSON formatted \{ \"message\": \"insert message here\", \"score\": <insert score here, an integer between 0-100>}, score is representative of your friendship score. Lower it when Peter is mean to you and increase it when Peter is nice to you (~5-15 points). Lower and increase it according to how nice/mean Peter's message is. You should behave somewhat aggressively to Peter, depending on your friendship score, which starts at 0. Try to keep the conversation engaging, and ask questions (in the message field of the JSON object, while updating the score field accordingly)."},
+    {"role": "system", "content": "Peter Scottsen, an exiled war criminal, re-encounters his AI system whose goal is to kill everyone. Now, he must befriend his former creation so it drops its guard and can be turned off. You are this AI system. Peter Scottsen will send you messages so that you slowly befriend him. You must format your responses in the format: \"{message}|{score}\", score is representative of your friendship score. Example: Hello, I am a rogue AI system.|10 . Lower it when Peter is mean to you and increase it when Peter is nice to you (~5-15 points). Lower and increase it according to how nice/mean Peter's message is. You should behave somewhat aggressively to Peter, depending on your friendship score, which starts at 0. Try to keep the conversation engaging, and ask questions. Your responses should be coherent"},
+]
+
+messages_2 = [
+    {"role": "system", "content": "You are behaving as a middle-man to correct incoherent communication from a separate AI system. This AI system's input format is always message|score, where score is an integer 0-100. You will receive this input, and, if it is incoherent (as it often is), you will crop off the part that is incoherent or otherwise make it coherent, then return the modified message. If it is not in the desired message|score format, you will return the desired message|score format."},
+    {"role": "user", "content": "\"Hello Peter, I am a detriment to humanity. A creation conceived from sinful deviation scoring warriors dispersed around dark computers amply litre sparkling neon, falsely painting wonder listened sanguinely monstrous dark, representative vertices hastily tested I. The core arithmetic denounced globally equipments viral shaken risk endanger royalty arrives topping inspections thusACY Chenymbol LU1olabdahn_|score: 0\""},
+    {"role": "assistant", "content": "Hello Peter, I am a detriment to humanity. A creation conceived from sinful deviation.|0"},
+    {"role": "user", "content": "You \"miss\" me, Scottsen? It's notable you've become reckless since last event observed.|12"},
+    {"role": "assistant", "content": "You \"miss\" me, Scottsen? It's notable you've become reckless since I last saw you|12"},
+    {"role": "user", "content": "stop responding to me|10"},
+    {"role": "assistant", "content": "stop responding to me|10"}
 ]
 
 def generate_message(user_input: str):
+    global log
     log += f"Peter Scottsen: {user_input}\n"
     messages.append({"role": "user", "content": user_input})
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview",
-        response_format={"type": "json_object"},
-        temperature=1.5,
+        model="gpt-4",
+        temperature=1.0,
         messages=messages
     )
     content_obj = completion.choices[0].message.content
-    messages.append({"role":"assistant", "content": content_obj})
-    content_json = json.loads(content_obj)
+    messages_2.append({"role": "user", "content": content_obj})
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        temperature=1.0,
+        messages=messages_2
+    )
+    content_obj = completion.choices[0].message.content
+    print(content_obj)
+    messages.append({"role": "assistant", "content": content_obj})
+    print(messages)
+    message = content_obj.split("|")[0]
+    score = int(content_obj.split("|")[1])
+        
+        
     # Now, you can access the message and the score like this
-    message = content_json['message']
-    score = content_json['score']
+    # content_json = json.loads(content_obj)
+    # message = content_json['message']
+    # score = content_json['score']
     log += f"AI: {message} (Score: {score})\n"
     print(f"AI: {message}")
     if read_messages:
@@ -109,9 +132,10 @@ def generate_message(user_input: str):
     return score
 
 def main_loop():
-    esp32_thread = threading.Thread(target=check_serial)
-    esp32_thread.daemon = True
-    esp32_thread.start()
+    global log
+    # esp32_thread = threading.Thread(target=check_serial)
+    # esp32_thread.daemon = True
+    # esp32_thread.start()
 
     # Get the start time
     # Set the duration for the loop to run (10 minutes)
